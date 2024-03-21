d2b0717349acd538ca15b56c8bf1b4e3d852bd16
Makheth <makhethamamoabi2@gmail.com>
2024-03-21 04:17:00 +0200

    Take home test

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import UserDetails from './UserDetails';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <View style={styles.headertext}>
+        <Text style={styles.head}>LIMKOKWING UNIVERSITY </Text>
+        <Text style={styles.subtext}>OF CREATIVE TECHNOLOGY</Text>
+      </View>
+
+      <View style={styles.textAndImageContainer}>
+        <Image
+          source={require('./me.png')}
+          style={styles.profilepicture}
+        />
+        <View style={styles.textContainer}>
+          <UserDetails label="StudentName: " value="Sophia Daniels" marginBottom={2} />
+          <UserDetails label="Student Number: " value="901016671" marginBottom={2}/>
+          <UserDetails label="Faculty: " value="FINT" marginBottom={2}/>
+          <UserDetails label="Semester:  " value="2 of year 2" marginBottom={2}/>
+          
+         
+          <StatusBar style="auto" />
+        </View>
+      </View>
+      <View>
+            <Text style={styles.textcontent}>SEMESTER GRADES</Text>
+            <Text>Mobile Device Programming :  A+</Text>
+            <Text>Probabilty and Statistics :  A-</Text>
+            <Text>Java Programming : B</Text>
+            <Text>Database Systems : B+</Text>
+            
+
+      </View>
     </View>
   );
 }
@@ -14,7 +42,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 15,
   },
+  headertext:{
+    alignItems:'center',
+    backgroundColor:'black',
+    marginBottom:10
+    
+  },
+  head:{
+    backgroundColor:'black',
+    color:'white',
+    fontSize: 20,
+    
+
+  },
+  subtext:{
+    color:'white'
+  },
+  textAndImageContainer: {
+    flexDirection: 'row', 
+    alignItems: 'flex-start', 
+  },
+  profilepicture: {
+    height: 100,
+    width: 85,
+    borderRadius: 2,
+    marginRight: 15,
+  },
+
+  textcontent:{
+    fontWeight: 500,
+    alignItems:'flex-start',
+    justifyContent: 'flex-start'
+  }
 });
